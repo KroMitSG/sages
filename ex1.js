47719791de1a8469f75fe5902f762f9f44898130
Michał Socha <MSocha@sygnity.pl>
2025-05-10 12:27:19 +0200

    Dodany plik ex1

---
+++ ex1.js
@@ -0,0 +1,50 @@
+function addEmployee(){
+    // create the user object and give the role
+    const user = {
+        firstName: 'Rory',
+        lastName: 'Millar',
+        role: 'Admin'
+    }
+ 
+    // add the new user to the database - and log out the response or error
+    axios.post('/user', user)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+}
+function addManager(){
+    // create the user object and give the role
+    const user = {
+        firstName: 'James',
+        lastName: 'Marley',
+        role: 'Admin'
+    }
+    // add the new user to the database - and log out the response or error
+    axios.post('/user', user)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+}
+function addAdmin(){
+    // create the user object and give the role
+    const user = {
+        firstName: 'Gary',
+        lastName: 'Judge',
+        role: 'Admin'
+    }
+ 
+    // add the new user to the database - and log out the response or error
+    axios.post('/user', user)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+}
